2a1a2b55dba70bc705bc4d577cbf11210e9aa83e
Valery <iofjuupasli@gmail.com>
2020-09-03 20:56:57 +0300

    Update todolist.js

--- day03/todolist.js
+++ day03/todolist.js
@@ -1,13 +1,36 @@
-let arr = [];
+const todos = [];
+
+let globalId = 0;
+
+function getId() {
+    globalId++;
+    return globalId;
+}
+
 function addTodo(newTodo) {
-    arr.push({ title: newTodo, status: "unchecked" });
+    todos.push({ id: getId(), title: newTodo, isComplete: false });
 }
+
 function removeTodo(todoId) {
-    arr.splice(todoId, 1)
+    const index = todos.findIndex((todo) => todo.id === todoId);
+    if (index !== -1) {
+        throw new Error('Id not found');
+    }
+    todos.splice(index, 1)
 }
+
 function changeTodoTitle(todoId, newTitle) {
-    arr[todoId].title = newTitle;
+    const index = todos.findIndex((todo) => todo.id === todoId);
+    if (index !== -1) {
+        throw new Error('Id not found');
+    }
+    todos[index].title = newTitle;
 }
+
 function changeTodoStatus(todoId, isComplete) {
-    arr[todoId].status = isComplete;
+    const index = todos.findIndex((todo) => todo.id === todoId);
+    if (index !== -1) {
+        throw new Error('Id not found');
+    }
+    todos[index].status = isComplete;
 }
